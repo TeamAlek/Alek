8725a5655546fe1e6980bced0b55f20b5fcc177c
森田浩平 <morita.aislab@gmail.com>
2015-11-09 17:20:08 +0900

    AlekからのRepositoryに移動｡ 他の2人からPushできるかは要検証｡

---
+++ Assets/Script/Top.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Top : MonoBehaviour {
+
+    GameObject tower;
+    float theta = 0.1f;
+	// Use this for initialization
+	void Start () {
+        tower = GameObject.Find("Tower");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tower.transform.Rotate(0, 1, 0, Space.World);
+    }
+}
---
+++ Assets/Script/WebCam.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class WebCam : MonoBehaviour {
+
+    private const string URL = "http://133.19.61.106:40005";
+    private const int WIDTH = 640;
+    private const int HEIGHT = 480;
+    private const int FPS = 30;
+
+    private Rect screenRect;
+    public WebCamTexture camTex;
+    public Texture2D texture;
+    public Color32[] img;
+
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(screenRect, camTex, ScaleMode.ScaleToFit);
+    }
+
+    // Use this for initialization
+    void Start() {
+        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        camTex = new WebCamTexture();
+        camTex.requestedHeight = WIDTH;
+        camTex.requestedWidth = HEIGHT;
+        camTex.Play();
+    }
+	
+	// Update is called once per frame
+	void Update() {
+        img = camTex.GetPixels32();
+        Texture2D texture = new Texture2D(camTex.width, camTex.height);
+        texture.SetPixels32(img);
+
+        //send screenshot
+        if (Input.GetKeyDown(KeyCode.Space)){
+            //convert
+            Texture2D tex2d = new Texture2D(camTex.width, camTex.height);
+            tex2d.SetPixels32(img);
+            byte[] bytes = tex2d.EncodeToPNG();
+
+            //send data
+            StartCoroutine("accessToServer", bytes);
+            //accessToServer(bytes);
+
+            print("finish capture and send");
+        }
+    }
+
+    private IEnumerator accessToServer(byte[] send_bytes)
+    {
+        var form = new WWWForm();
+        form.AddBinaryData("fileUpload", send_bytes, "send.png", "image/png");
+        var www = new WWW(URL, form);
+
+        //受信
+        yield return www; //受信待機
+        if (www.error == null)
+        {
+            if (www.text != "null")
+                print("string : " + www.text);
+            else
+                print("QRcode not found");
+        }
+    }
+
+}
+
+
