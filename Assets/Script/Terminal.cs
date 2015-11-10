2b951ab6d12675dfaea7b605b82d01a821ded8e1
Nakata <is0127hx@ed.ristumei.ac.jp>
2015-11-10 14:22:39 +0900

    LogoSceneからTerminalシーンへの遷移を実装

---
+++ Assets/Script/Terminal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Terminal : MonoBehaviour {
+
+    GameObject character;
+    float theta = 0.1f;
+    // Use this for initialization
+    void Start () {
+        character = GameObject.Find("Character");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        character.transform.Rotate(0, 1, 0, Space.World);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 ﻿# Alek
 
 MobileSystem特論のTeam用Repositoryﾃﾞｽyo
+
+## 使用素材
+
+###フォント
+
+*itouhiroはてなブログ
+http://itouhiro.hatenablog.com/entry/20130602/font
+
+*test
